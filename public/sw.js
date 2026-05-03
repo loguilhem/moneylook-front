ad763497105a07f3ece71ef207e8c161bac04c5d
loguilhem <guilhem.vilarroya@protonmail.com>
2026-05-03 12:18:22 +0200

    pwa

---
+++ public/sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'moneylook-shell-v1'
+const APP_SHELL = [
+  '/',
+  '/manifest.webmanifest',
+  '/favicon.svg',
+  '/icons.svg',
+  '/moneylook-icon.png',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName))),
+      ),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event
+
+  if (request.method !== 'GET') {
+    return
+  }
+
+  const requestUrl = new URL(request.url)
+  if (requestUrl.origin !== self.location.origin) {
+    return
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).catch(() => caches.match('/')),
+    )
+    return
+  }
+
+  if (!['script', 'style', 'image', 'font', 'manifest'].includes(request.destination)) {
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+
+      return fetch(request).then((response) => {
+        if (!response || response.status !== 200) {
+          return response
+        }
+
+        const responseClone = response.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone))
+        return response
+      })
+    }),
+  )
+})
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 import { router } from './app/router'
+import { registerServiceWorker } from './registerServiceWorker'
 import './i18n/i18n'
+
+registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
